2e9e9c735f36208c1d302d0bbb34357f49675916
Elelion <elelion@yandex.ru>
2020-02-09 11:55:11 +0300

    Создаем поля через ActiveForm тем самым передаем данные в модель

--- views/post/test.php
+++ views/post/test.php
@@ -1,3 +1,8 @@
+<?php
+use yii\widgets\ActiveForm;
+use yii\helpers\Html;
+?>
+
 <h1>Test Action</h1>
 
 <?php
@@ -13,4 +18,60 @@
  * вызываем нашу глобальную ф-цию из глобального файла ./functions.php
  * что находиться в корне проекта
  * */
-debug(Yii::$app);
+//debug(Yii::$app);
+//debug($model);
+?>
+
+<!-- NOTE: создаем форму, импользуя ActiveForm & ActiveField -->
+<?php $form = ActiveForm::begin(['options' => ['id' => 'testForm']]) ?>
+<?= $form->field($model, 'name')->label('Имя') ?>
+<?= $form->field($model, 'email')->input('email') ?>
+<?= $form->field($model, 'text')
+        ->label('Текст сообщения')
+        ->textarea(['rows' => 10]) ?>
+
+<?= Html::submitButton('Отправить', ['class' => 'btn btn-success']) ?>
+
+<?php ActiveForm::end() ?>
+
+<!--
+NOTE:
+activeForm - позволяет начать создание формы
+activeField - позволяет настроить поля нашей формы
+Html - это хелпер HTML, нужен для создания HTML элементов (кнопка, и.т.п.)
+-->
+
+<!--
+NOTE:
+$form = ActiveForm::begin() - объявляем начало создание формы
+ActiveForm::end() - закрываем нашу форму
+-->
+
+<!--
+NOTE:
+• $form->field() - принимает три параметра
+1. - модель
+2. атрибут данной модели, т.е. имя поля которое мы создаем
+Например:
+$form->field($model, 'text') - создаст input + label
+
+• ...->label('Имя') - для создания label, которому будет присвоино наше название
+• ...->textarea() - создание текстового поля
+• ...->passwordInput() - задаем поле как для ввода пароля, под ****
+• ...->input('email') - задает поле как поле для ввода eMail's
+
+и.т.п. методы, см. в офф. доке
+https://www.yiiframework.com/doc/api/2.0/yii-widgets-activefield
+-->
+
+<!--
+NOTE:
+Html::submitButton() - создаст кнопку, где первый параметр принимает
+надпись на кнопке, а вторым - массив опций
+-->
+
+<!--
+NOTE:
+Все эти формы нужны так же для того, что бы проверять ошибки валидации и.т.п.
+не только на стороне сервера но и на стороне клиента
+-->
