48eaffcb9c5bac6ae48868aa97bf2b8c028c13ba
Elelion <elelion@yandex.ru>
2020-02-16 23:39:45 +0300

    Проверяем модель на загрузку данных и работает м флешом(сессия)

--- controllers/PostController.php
+++ controllers/PostController.php
@@ -40,23 +40,34 @@ class PostController extends AppController
     public function actionIndex()
     {
         // NOTE: если, был передан ajax запрос, то...
-        if (Yii::$app->request->isAjax) {
-            return 'test';
+        // if (Yii::$app->request->isAjax) {
+
+        //NOTE: если данные были переданы методом $_POST
+        if (Yii::$app->request->post()) {
+            // return 'test';
         }
 
         // NOTE: создаем объект нашей модели
         $model = new TestForm();
 
-        $names = [
-            'Ivanov',
-            'Petrov',
-            'Sidorov'
-        ];
+        // NOTE: если в модель удалось загрузить данные
+        if ($model->load(Yii::$app->request->post())) {
+            // debug(Yii::$app->request->post());
+            // debug($model);
 
-//        $this->debug(Yii::$app);
+            // NOTE: если все правила валидации успешно пройдены
+            if ($model->validate()) {
+                //NOTE: устанавливаем флеш (ключ, значение)
+                Yii::$app->session->setFlash('success', 'Flash данные приняты');
+            } else {
+                Yii::$app->session->setFlash('error', 'Flash ошибка');
+            }
+        }
+
+        // $this->debug(Yii::$app);
 
         // NOTE: передаем объект нашей модели compact('model')
-        return $this->render('test', compact('model'));
+        return $this->render('test', compact('model', 'temp'));
     }
 
     // NOTE: создаем action где более 1го слова в названии
@@ -92,3 +103,10 @@ class PostController extends AppController
         return $this->render('show');
     }
 }
+
+/*
+NOTE:
+Флеш сообщения - это сообщения которые записываются в сессию, но не остаются
+там, а являются как бы одноразывыми. Т.е. сразу же после того как мы их
+запросили они, будут удалены из сессии
+*/
