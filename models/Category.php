9e5f969a9af9ab7863a3c10b90a6e9a0eb4d45c1
Elelion <elelion@yandex.ru>
2020-02-22 19:33:51 +0300

    Создаем класс ActiveRecord для нашей созданной БД

--- models/Category.php
+++ models/Category.php
@@ -3,8 +3,19 @@
 
 namespace app\models;
 
+use yii\db\ActiveRecord;
 
-class Category
+/*
+ * NOTE:
+ * yii2 автоматически попытается связать Category с таблицей Category, но
+ * т.к. у нас таблица называется Categories , то будет ошибка
+ * Table 'yii2_basic_courses.category' doesn't exist
+ * */
+class Category extends ActiveRecord
 {
-
+    public static function tableName()
+    {
+        // NOTE: указываем наше имя таблици
+        return 'categories';
+    }
 }
