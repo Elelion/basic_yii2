e2cbbdac06781fa3cc6ffee9863550bf54116f6a
Elelion <elelion@yandex.ru>
2020-02-29 09:47:47 +0300

    Добавили новые запросы

--- queries.sql
+++ queries.sql
@@ -1,15 +1,15 @@
 INSERT INTO categories (title,	parent, alias) VALUES
-    ('Комплектующие к Apple', 0, 'lompletuyshie-k-apple'),
-    ('Запчасти iPad', 685, 'zapchasti-ipad'),
-    ('Запчасти iPhone', 685, 'zapchasti-iPhone'),
-    ('Запчасти iPod', 685, 'zapchasti- iPod'),
-    ('iPad', 691, 'ipad'),
-    ('iPad 2', 691, 'ipad-2'),
-    ('iPad NEW (iPad 3)', 691, 'ipad-new-ipad-3');
+    ('Комплектующие к Apple', 1, 'lompletuyshie-k-apple'),
+    ('Запчасти iPad', 2, 'zapchasti-ipad'),
+    ('Запчасти iPhone', 3, 'zapchasti-iPhone'),
+    ('Запчасти iPod', 4, 'zapchasti- iPod'),
+    ('iPad', 5, 'ipad'),
+    ('iPad 2', 6, 'ipad-2'),
+    ('iPad NEW (iPad 3)', 7, 'ipad-new-ipad-3');
 
-INSERT INTO categories (title, alias, parent, content) VALUES
-    ('tLCD ipod Touch', 'lcd-ipod-touch', 693, 'tlcd'),
-    ('Len+Touchscreen iPod Touch', 'len-touchscreen-ipod-touch', 693, ''),
-    ('Аккумулятор iPod Touch 1G', 'akkumulator-ipod-touch-1g', 693, 'touch 1'),
-    ('Аккумулятор iPod Touch 2G', 'akkumulator-ipod-touch-2g', 693, ''),
-    ('Len+Touchscreen iPod Touch 2G', 'len-touchscreen-ipod-touch-2g', 693, '');
+INSERT INTO products (title, alias, parent, content) VALUES
+    ('tLCD ipod Touch', 'lcd-ipod-touch', 1, 'tlcd'),
+    ('Len+Touchscreen iPod Touch', 'len-touchscreen-ipod-touch', 2, ''),
+    ('Аккумулятор iPod Touch 1G', 'akkumulator-ipod-touch-1g', 3, 'touch 1'),
+    ('Аккумулятор iPod Touch 2G', 'akkumulator-ipod-touch-2g', 4, ''),
+    ('Len+Touchscreen iPod Touch 2G', 'len-touchscreen-ipod-touch-2g', 5, '');
