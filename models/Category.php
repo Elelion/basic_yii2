e09e33dd675dadf5e355635d8b76c5f55007c153
Elelion <elelion@yandex.ru>
2020-02-29 11:34:24 +0300

    Убрали комментарии не относящиеся к данному разделу

--- models/Category.php
+++ models/Category.php
@@ -5,27 +5,15 @@ namespace app\models;
 
 use yii\db\ActiveRecord;
 
-/*
- * NOTE:
- * yii2 автоматически попытается связать Category с таблицей Category, но
- * т.к. у нас таблица называется Categories , то будет ошибка
- * Table 'yii2_basic_courses.category' doesn't exist
- * */
 class Category extends ActiveRecord
 {
     public static function tableName()
     {
-        // NOTE: указываем наше имя таблици
         return 'categories';
     }
 
     public function getProducts()
     {
-        /*
-         * NOTE:
-         * связь один ко многим, принимает имя класса и массив
-         *  hasMany - т.к. к одной категории может относиться несколько продуктов
-         * */
         return $this->hasMany(Product::className(), ['parent' => 'id']);
     }
 }
--- models/Product.php
+++ models/Product.php
@@ -10,13 +10,6 @@ class Product extends ActiveRecord
 {
     public static function tableName()
     {
-        // NOTE: указываем наше имя таблици
         return 'categories';
     }
-
-    // NOTE: один продукт относиться только к одной категрии, по этому hasOne
-    //public function getCategories()
-    //{
-    //    return $this->hasOne(Category::className(), ['id' => 'parent']);
-    //}
 }
