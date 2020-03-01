d3c8eff88d0bfe7ec663d6ea371acbd7a571bb7f
Elelion <elelion@yandex.ru>
2020-03-01 12:06:57 +0300

    Выводим наш виджет

--- views/post/show.php
+++ views/post/show.php
@@ -1,3 +1,8 @@
+<?php
+// NOTE: подключаем наш виджет
+use app\components\MyWidget;
+?>
+
 <?php /** @var TYPE_NAME $cats */ ?>
 
 <?php $this->beginBlock('block1'); ?>
@@ -6,6 +11,29 @@
 <?php $this->endBlock(); ?>
 
 <h1>Show Action Content</h1>
+
+<hr>
+<!--
+NOTE:
+выводим наш виджет, вывод может быть как через созданный нами метод
+MyWidget::run();
+либо через вызов самого виджета, где по умолчанию всегда будет вызван
+метод run, т.е. так: MyWidget::widget();
+-->
+
+<!--
+NOTE:
+Так же мы можем передавать парметры в наш виджете, а если мы его не передадим,
+то будет использован параметр по умолчанию
+MyWidget::widget(); см. MyWidget->init
+-->
+<?php echo MyWidget::widget(['name' => 'Вася']); ?>
+
+<?php MyWidget::begin() ?>
+  <h1>контент -> show.php -> MyWidget::begin</h1>
+<?php MyWidget::end() ?>
+
+<hr>
 
 <?php
     foreach ($cats as $cat) {
